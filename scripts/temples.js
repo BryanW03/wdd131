ce7189b00fdc5b70beb25c478c3ef7312dd2ac93
BryanW03 <bryanw628@gmail.com>
2025-05-11 16:21:24 -0600

    week2

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const currentYearSpan = document.getElementById("currentyear");
-    const lastModifiedParagraph = document.getElementById("lastModified");
-
-
-    const currentYear = new Date().getFullYear();
-
-
-    currentYearSpan.textContent = currentYear;
-
-
-    const lastModifiedDate = document.lastModified;
-
-    lastModifiedParagraph.textContent = "Las Modified: " + lastModifiedDate;
-});
 
 
 document.addEventListener('DOMContentLoaded', function () {
